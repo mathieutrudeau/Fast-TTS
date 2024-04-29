2f7218796a14f20dca6199bc5fd5598976d3b4d8
Mathieu Trudeau <mathieutrudeau11@gmail.com>
2024-04-29 11:24:26 -0400

    Removed initial code
    This code will be changed to be more concise.

---
+++ src/Fast_TTS/fast_tts.py
@@ -0,0 +1,18 @@
+
+
+
+
+def fast_tts(text, delimiter='', speaker='Default', language='en', use_tortoise=True, 
+                     index_rate=0.5, filter_radius=3, resample_sr=0, rms_mix_rate=0.25, protect=0.33):
+    try:
+        print('Fast TTS')
+
+        print('Text: ', text)
+
+
+
+
+    except Exception as e:
+        print('Error in Fast TTS: ', e)
+
+fast_tts('Hello World!')
